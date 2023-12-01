37b81d9384813a034a2f8152fc39d94e1e10186c
Jigar-gokani <jigar.gokani@somaiya.edu>
2023-12-02 03:17:13 +0530

    setCurr Chart

--- src/components/core/Dashboard/InstructorDashboard/InstructorChart.jsx
+++ src/components/core/Dashboard/InstructorDashboard/InstructorChart.jsx
@@ -46,3 +46,16 @@ export default function InstructorChart({ courses }) {
    const options = {
     maintainAspectRatio: false,
   }
+  return (
+    <div className="flex flex-1 flex-col gap-y-4 rounded-md bg-richblack-800 p-6">
+      <p className="text-lg font-bold text-richblack-5">Visualize</p>
+      <div className="space-x-4 font-semibold">
+        {/* Button to switch to the "students" chart */}
+        <button
+          onClick={() => setCurrChart("students")}
+          className={`rounded-sm p-1 px-3 transition-all duration-200 ${
+            currChart === "students"
+              ? "bg-richblack-700 text-yellow-50"
+              : "text-yellow-400"
+          }`}
+        >
